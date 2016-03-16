d97fecca0f2af32739d1767bfbb38d83653098a4
Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>
2016-03-16 13:24:51 +0200

    Use states from hadoop-client. Use get_dist_config from lib.

--- reactive/pig.py
+++ reactive/pig.py
@@ -1,26 +1,13 @@
-import jujuresources
 from charms.reactive import when, when_not
 from charms.reactive import set_state, remove_state
 from charmhelpers.core import hookenv
-from jujubigdata import utils
-from charmhelpers.fetch import apt_install
-from subprocess import check_call
 from charms.pig import Pig
-
-DIST_KEYS = ['vendor', 'hadoop_version', 'groups', 'users', 'dirs']
-
-def get_dist_config(keys):
-    from jujubigdata.utils import DistConfig
-
-    if not getattr(get_dist_config, 'value', None):
-        get_dist_config.value = DistConfig(filename='dist.yaml', required_keys=keys)
-    return get_dist_config.value
+from charms.layer.hadoop_client import get_dist_config
 
 
-#@when('hadoop.installed')
 @when_not('pig.installed')
 def install_pig():
-    pig = Pig(get_dist_config(DIST_KEYS))
+    pig = Pig(get_dist_config())
     if pig.verify_resources():
         hookenv.status_set('maintenance', 'Installing Pig')
         pig.install()
@@ -28,7 +15,7 @@ def install_pig():
 
 
 @when('pig.installed')
-@when_not('hadoop.related')
+@when_not('hadoop.ready')
 def missing_hadoop():
     hookenv.status_set('blocked', 'Waiting for relation to Hadoop')
 
@@ -37,7 +24,7 @@ def missing_hadoop():
 @when_not('pig.configured')
 def configure_pig(*args):
     hookenv.status_set('maintenance', 'Setting up pig')
-    pig = Pig(get_dist_config(DIST_KEYS))
+    pig = Pig(get_dist_config())
     pig.setup_pig()
     set_state('pig.configured')
     hookenv.status_set('active', 'Ready')
